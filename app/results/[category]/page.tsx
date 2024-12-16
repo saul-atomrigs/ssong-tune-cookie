31da8593551cc66b33e6078a084a602b72a9a7d7
Saul Lee <solleedata@gmail.com>
2024-12-16 18:26:15 +0900

    fix: update sharing functionality to use a static URL instead of selected song's YouTube link

--- app/results/[category]/page.tsx
+++ app/results/[category]/page.tsx
@@ -33,14 +33,12 @@ const ResultPage = () => {
   }, [category]);
 
   const handleShare = async () => {
-    const youtubeLink = selectedSong?.youtubeUrl;
-
     if (navigator.share) {
       try {
         await navigator.share({
           title: '2025년 내 운세 보러가기',
           text: '새해 나의 운세를 확인하고, 나에게 맞는 노래를 들어보세요!',
-          url: youtubeLink,
+          url: 'https://ssong-tune-cookie.vercel.app/',
         });
       } catch (error) {
         console.log('Share Error:', error);
