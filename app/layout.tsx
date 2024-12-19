9ac00fb4570a8fdde9527dc1d711ad605cc25015
Saul Lee <solleedata@gmail.com>
2024-12-19 13:41:10 +0900

    feat: add fortune cookie image to Open Graph metadata in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,14 @@ export const metadata: Metadata = {
   description: '새해 운세를 음악으로 들어보세요!',
   openGraph: {
     description: '2025년 내가 원하는 것을 얻게 해줄 노래는?',
+    images: [
+      {
+        url: '/fortune-cookie.svg',
+        width: 160,
+        height: 154,
+        alt: '포춘쿠키',
+      },
+    ],
   },
 };
 
