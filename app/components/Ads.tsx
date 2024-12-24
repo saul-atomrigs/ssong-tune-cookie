5c2e85c42c8df0e4d124576443c3c122e2d16a8f
Saul Lee <solleedata@gmail.com>
2024-12-24 17:57:23 +0900

    feat: implement AdFit component with dynamic ad script loading

--- app/components/Ads.tsx
+++ app/components/Ads.tsx
@@ -1,3 +1,24 @@
-export default function Ads() {
-  return <div className='w-full h-14 bg-[#D9D9D9] p-4'></div>;
-}
+import { useEffect } from 'react';
+
+const AdFit = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const adScript = document.createElement('script');
+      adScript.src = '//t1.daumcdn.net/kas/static/ba.min.js';
+      adScript.async = true;
+      document.body.appendChild(adScript);
+    }
+  }, []);
+
+  return (
+    <ins
+      className='kakao_ad_area'
+      style={{ display: 'none' }}
+      data-ad-unit='DAN-JWxtPuO6RBoTT1tA'
+      data-ad-width='320'
+      data-ad-height='100'
+    ></ins>
+  );
+};
+
+export default AdFit;
