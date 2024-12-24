bcaf813e6e6f2d45fb505c4f35631ad63ba7607a
Saul Lee <solleedata@gmail.com>
2024-12-24 22:39:30 +0900

    feat: update Kakao initialization with hardcoded API key for testing

--- app/KakaoScript.tsx
+++ app/KakaoScript.tsx
@@ -4,7 +4,8 @@ import Script from 'next/script';
 
 function KakaoScript() {
   const onLoad = () => {
-    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+    // window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+    window.Kakao.init('9e35ef94a17fdccf780a8b7bd2e8f3ce');
   };
 
   return (
