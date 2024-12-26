06d55447681eed9dd5848954d8f7f56e7a60eb81
Saul Lee <solleedata@gmail.com>
2024-12-26 17:05:38 +0900

    feat: update responsive design for header and button, and add new sun motion JSON asset

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -10,22 +10,23 @@ const App = () => {
     <div className='relative h-[100dvh]'>
       <div className='h-full flex flex-col'>
         <div className='flex-1 flex flex-col items-center justify-center gap-2'>
-          <h1 className='text-5xl sm:text-7xl [@media(max-height:600px)]:text-3xl font-bold text-center font-[ChangwonDangamAsac] text-[#FFFBE6] leading-tight mb-2'>
+          <h1 className='text-5xl sm:text-7xl [@media(max-height:750px)]:text-3xl font-bold text-center font-[ChangwonDangamAsac] text-[#FFFBE6] leading-tight mb-2'>
             2025 <br /> 쏭-춘쿠키
           </h1>
-          <div className='relative w-[100vw] sm:w-[80vw] md:w-[70vw] lg:w-[60vw] [@media(max-height:600px)]:w-[60vw] aspect-[4/3] [@media(max-height:500px)]:aspect-[5/3]'>
+          <div className='w-[100vw] sm:w-[80vw] md:w-[70vw] lg:w-[60vw] [@media(max-height:750px)]:w-[60vw] aspect-[4/3] [@media(max-height:500px)]:aspect-[5/3] flex items-center justify-center'>
             <Image
               src='/snake.svg'
               alt='Snake'
-              fill
-              className='object-contain'
+              width={800}
+              height={600}
+              className='w-full h-full object-contain'
             />
           </div>
-          <p className='text-md sm:text-xl [@media(max-height:600px)]:text-sm text-[#FFFBE6] text-center font-medium font-[Paperlogy] px-4'>
+          <p className='text-md sm:text-xl [@media(max-height:750px)]:text-sm text-[#FFFBE6] text-center font-medium font-[Paperlogy] px-4 mt-2'>
             2025년 내가 가장 원하는 것을 <br />
             얻게 해줄 노래를 들어보세요
           </p>
-          <div className='w-full max-w-[500px] [@media(max-height:600px)]:max-w-[300px] px-4 sm:px-6 py-3 mt-2'>
+          <div className='w-full max-w-[500px] [@media(max-height:750px)]:max-w-[300px] px-4 sm:px-6 py-3 mt-2'>
             <Button onClick={() => router.push('/login')} variant='start'>
               뽑기 시작하기
             </Button>
