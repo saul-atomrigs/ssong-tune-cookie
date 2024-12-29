2918ed1910ff11f23a8867fcacb72f089509697c
Saul Lee <solleedata@gmail.com>
2024-12-29 15:02:45 +0900

    feat: add Open Graph title for New Year theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ export const metadata: Metadata = {
   title: '나를 대박나게 해줄 새해 노래 듣고 2025년 시작하세요',
   description: '새해 운세를 음악으로 들어보세요!',
   openGraph: {
+    title: '나를 대박나게 해줄 새해 노래 듣고 2025년 시작하세요',
     images: [
       {
         url: '/open-graph-original.png',
