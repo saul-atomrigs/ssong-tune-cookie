d66319187a5df2b4a589ab1826a799c4399e47b6
Saul Lee <solleedata@gmail.com>
2024-12-30 16:17:54 +0900

    style: update height properties for ListPage component to use min-h-[100vh] and h-screen

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -23,7 +23,7 @@ const ListPage = () => {
   };
 
   return (
-    <div className='flex flex-col h-[100dvh]'>
+    <div className='flex flex-col h-screen min-h-[100vh]'>
       <div className='h-full flex items-center justify-center p-3 [@media(max-height:500px)]:p-2 pb-24 [@media(max-height:500px)]:pb-16'>
         <div className='flex flex-col items-center space-y-7 [@media(max-height:500px)]:space-y-2 w-full max-w-sm'>
           <h1 className='text-2xl [@media(max-height:530px)]:text-xl font-bold text-center font-[Paperlogy] text-[#FFF8EC]'>
